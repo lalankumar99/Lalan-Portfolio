920791701460ee7c150a47e3fed812b9a28774c4
Lalan Kumar <lalankumarbnk17@gmail.com>
2026-09-21 22:09:47 +0530

    Update portfolio header in app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // =============================================
-// LALAN KUMAR - PREMIUM PORTFOLIO
+// LALAN KUMAR - ELECTRICAL ENGG. - PORTFOLIO 
 // Component Loader v3.0
 // =============================================
 
